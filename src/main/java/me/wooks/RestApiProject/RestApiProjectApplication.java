d263783fe327c8067c3660c9cf9952c4c807f205
cotlsdnr@hanmail.net <cotlsdnr@hanmail.net>
2019-09-26 14:21:50 +0900

    Restrict input for POST /api/events

--- src/main/java/me/wooks/RestApiProject/RestApiProjectApplication.java
+++ src/main/java/me/wooks/RestApiProject/RestApiProjectApplication.java
@@ -1,13 +1,20 @@
 package me.wooks.RestApiProject;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RestApiProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestApiProjectApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiProjectApplication.class, args);
+    }
+
+    @Bean
+	public ModelMapper modelMapper(){
+    	return new ModelMapper();
 	}
 
 }
